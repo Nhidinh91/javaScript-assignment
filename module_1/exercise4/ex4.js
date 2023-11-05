c25ec66b7ad57b4afa28b7fc782aef776e1330e1
NhiDinh <thaonhi6991@gmail.com>
2023-11-05 10:26:28 +0200

    update

--- module_1/exercise4/ex4.js
+++ module_1/exercise4/ex4.js
@@ -3,12 +3,12 @@ let roll = Math.floor(Math.random()*4)+1;
 if (roll == 1) {
   document.querySelector('#result').innerHTML = `${name}, you are a Gryffindor`;
 }
-if (roll == 2) {
+else if (roll == 2) {
   document.querySelector('#result').innerHTML = `${name}, you are a Slytherin`;
 }
-if (roll == 3) {
+else if (roll == 3) {
   document.querySelector('#result').innerHTML = `${name}, you are a Hufflepuff`;
 }
-if (roll == 3) {
+else if (roll == 4) {
   document.querySelector('#result').innerHTML = `${name}, you are a Ravenclaw`;
 }
