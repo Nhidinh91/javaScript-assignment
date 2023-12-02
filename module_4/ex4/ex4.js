253a709b8298c98f1cf4f7bdaa55ae92b0d9d3c0
NhiDinh <thaonhi6991@gmail.com>
2023-12-02 17:13:24 +0200

    update

--- module_4/ex4/ex4.js
+++ module_4/ex4/ex4.js
@@ -4,32 +4,37 @@ actionForm.addEventListener('submit', async function(e) {
   e.preventDefault();
   let searchKey = document.querySelector('#query').value;
   try {
-    const response = await fetch(
-        `https://api.tvmaze.com/search/shows?q=${searchKey}`);
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchKey}`);
     const jsonData = await response.json();
     let results = document.querySelector('#results');
     results.innerHTML = '';
+
     for (let data of jsonData) {
-      let h2 = document.createElement('h2');
+      let h2  = document.createElement('h2');
       h2.innerHTML = data.show.name;
+
       let a = document.createElement('a');
       a.setAttribute('href', data.show.url);
       a.setAttribute('target', '_blank');
       a.innerText = 'Link';
+
       let img = document.createElement('img');
-      if (data.show.image.medium == null){
-        img.setAttribute('src', 'https://via.placeholder.com/210x295?text=Not%20Found');
+      if (data.show.image && data.show.image.medium){
+        img.setAttribute('src', data.show.image.medium);
       } else {
-        img.setAttribute('src', data.show.image?.medium);
+        img.setAttribute('src', 'https://via.placeholder.com/210x295?text=Not%20Found');
       }
       img.setAttribute('alt', data.show.name);
+
       let div = document.createElement('div');
       div.innerHTML = data.show.summary;
+
       let article = document.createElement('article');
       article.appendChild(h2);
       article.appendChild(a);
       article.appendChild(img);
       article.appendChild(div);
+
       results.appendChild(article);
 
     }
